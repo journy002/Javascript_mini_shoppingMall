fd1888f5bae22b90ccfadecd943763cfcf25fe71
journy002 <journy002@naver.com>
2021-11-06 15:20:17 +0900

    update

--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
-// main
+// Fetch the items from the JSON file
+function loadItems() {
+    return fetch('data/data.json') // fetch로 데이터를 받아오고
+    .then(response => response.json()) // 성공적으로 데이터를 받아오면 그걸 json으로 바꿔준 다음
+    .then(json => json.items) // json안에 있는 items를 return
+}
 
+// main
 loadItems()
     .then(items => {
-        displayItems(items);
-        setEventListeners(items)
+        // displayItems(items);
+        // setEventListeners(items)
     })
     .catch(console.log());
 
